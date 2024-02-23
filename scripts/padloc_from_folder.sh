d5c5043cdae8237394f5128585ae67fd74682902
PKbioscience <bioscience.kp@gmail.com>
2024-02-24 00:13:10 +0300

    Update padloc scrips

---
+++ scripts/padloc_from_folder.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#v1.2
+# Выполняет поиск по всем подкаталогам
+# Для нуклеотидных последовательностей без аннотации
+# Специализирован под результаты загрузки с помощью ncbi-dataset
+# Аргументы:
+###1 - путь, по которому лежат результаты ncbi-dataset
+###2 - название папки для результатов
+
+# Пример ввода команды:
+# bash padloc_from_list.sh Downloads Results
+
+
+mkdir $2
+
+for VAR in $(ls $1/ncbi_dataset/data/ | grep '^GCF')
+do
+padloc --fna $1/ncbi_dataset/data/${VAR}/${VAR}*.fna --outdir $2
+done
+
